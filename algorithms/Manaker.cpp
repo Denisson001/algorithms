53941aa2ba36c71a70b3e515c6e16c1b37513c01
Николай <nmtretyakov@mail.ru>
2019-11-10 22:23:23 +0300

    Completed Manaker.cpp

--- algorithms/Manaker.cpp
+++ algorithms/Manaker.cpp
@@ -1,18 +1,22 @@
-vector <int> manaker(const string& s) {
-	vector <int> man(s.size(), 0);
-	int l = 0, r = 0;
-	int n = s.size();
-	for (int i = 1; i < n; i++) {
-		if (i <= r) {
-			man[i] = min(r - i, man[l + r - i]);
-		}
-		while (i + man[i] + 1 < n && i - man[i] - 1 >= 0 && s[i + man[i] + 1] == s[i - man[i] - 1]) {
-			man[i]++;
-		}
-		if (i + man[i] > r) {
-			l = i - man[i];
-			r = i + man[i];
-		}
-	}
-	return man;
+// для четных палиндромов:
+// 1) запустить со строкой вида a#b#a#c#a#b#a
+// 2) взять значения в позициях решеток
+vector<int> manaker(const string &s) {
+    vector<int> man(s.size(), 0);
+    int l = 0, r = 0;
+    int n = s.size();
+    for (int i = 1; i < n; i++) {
+        if (i <= r) {
+            man[i] = min(r - i, man[l + r - i]);
+        }
+        while (i + man[i] + 1 < n && i - man[i] - 1 >= 0
+               && s[i + man[i] + 1] == s[i - man[i] - 1]) {
+            man[i]++;
+        }
+        if (i + man[i] > r) {
+            l = i - man[i];
+            r = i + man[i];
+        }
+    }
+    return man;
 }
