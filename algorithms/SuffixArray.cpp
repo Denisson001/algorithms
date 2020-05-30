fe0455af0c11268b2765a6c031fa9f479ebe784e
DmitryGr <mitia810@mail.ru>
2020-05-30 14:27:31 +0300

    Update SuffixArray.cpp

--- algorithms/SuffixArray.cpp
+++ algorithms/SuffixArray.cpp
@@ -1,3 +1,6 @@
+// It's strictly forbidden to use buildSA() and not 
+// to add a fictitious char, which should be smaller 
+// than all other chars in a string, to the end!!!
 struct SuffixArray {
     static const int SZ = 3e5;
 
