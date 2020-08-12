c23b9c80900c248825e850047156d4a69d715acd
Denisson <shpakovskij.denis@yandex.ru>
2020-08-12 13:48:30 +0300

    Update Cartesian.cpp

--- algorithms/Cartesian.cpp
+++ algorithms/Cartesian.cpp
@@ -1,73 +1,108 @@
-#include <bits/stdc++.h>
-#define merge merg
-#define ll long long
- 
-using namespace std;
+#define merge abacaba
 
-//a cartesian tree is represented as just an index of the root in the global array
-//0 is a fictitious vertex here, don`t forget!
+// a cartesian tree is represented as just an index of the root in the global array
+// 0 is a fictitious vertex here, don`t forget!
 
-struct Vertex{int l; int r; int pr; int sz; int value;};
-const int INF = 1e9;
-const int N = 1e5+11; //possible number of vertex here
-Vertex decart[N];
-int ptr=0;
-
-int create_vertex(int value){ 
-	decart[ptr++] = {0, 0, rand()%1000000000, 1, value};
-	return ptr-1;
+int gen_priority() {
+  return rand() & ((1 << 25) - 1);
 }
 
-void update(int vertex){
-    int L = decart[vertex].l, R = decart[vertex].r;
-    decart[vertex].sz = 1+decart[L].sz+decart[R].sz;
+template<typename T>
+struct Vertex {
+  int l;
+  int r;
+  int pr; // priority
+  int sz;
+  int father;
+  T value;
+};
+
+const int N = 1e6 + 11; // possible number of vertex here
+Vertex<int> tr[N];      // set type of value here
+int ptr = 0;
+
+template<typename T>
+int create_vertex(T value) {
+  tr[ptr] = {0, 0, gen_priority(), 1, -1, value};
+  return ptr++;
 }
 
-pair<int, int> split(int father, int number){ //it lefts number elements within the left node and the remainings in the right one.
-    if (father <= 0) return make_pair(0, 0);
-    int L = decart[father].l, R = decart[father].r;
-    int l = 1+decart[L].sz;
-    if (l <= number){
-        pair<int, int> p = split(R, number - l);
-        decart[father].r = p.first;
-        p.first = father;
-        update(father);
-        return p;
+void update(int v) {
+  int L = tr[v].l, R = tr[v].r;
+  tr[v].sz = 1 + tr[L].sz + tr[R].sz;
+  // update value here like this
+  // tr[v].total_value = min({tr[v].value, tr[L].total_value, tr[R].total_value});
+}
+
+pair<int, int> split(int father, int number) { // it lefts number elements within the left node and the remainings in the right one.
+  if (father <= 0) return make_pair(0, 0);
+  int L = tr[father].l, R = tr[father].r;
+  int l = 1 + tr[L].sz;
+  if (l <= number) {
+    tr[R].father = -1;
+    pair<int, int> p = split(R, number - l);
+    tr[father].r = p.first;
+    tr[p.first].father = father;
+    p.first = father;
+    update(father);
+    return p;
+  } else {
+    tr[L].father = -1;
+    pair<int, int> p = split(L, number);
+    tr[father].l = p.second;
+    tr[p.second].father = father;
+    p.second = father;
+    update(father);
+    return p;
+  }
+}
+
+int merge(int first, int second) { // merges two cartesians having roots first and second
+  if (first <= 0) return second;
+  if (second <= 0) return first;
+  if (tr[first].pr >= tr[second].pr) {
+    tr[tr[first].r].father = -1;
+    int v = merge(tr[first].r, second);
+    tr[first].r = v;
+    tr[v].father = first;
+    update(first);
+    return first;
+  } else {
+    tr[tr[second].l].father = -1;
+    int v = merge(first, tr[second].l);
+    tr[second].l = v;
+    tr[v].father = second;
+    update(second);
+    return second;
+  }
+}
+
+int get_left(int base) { // get amount of vertex to the left of base
+  int R = 0;
+  int last = -1;
+  while (base > 0) {
+    if (tr[base].l > 0 && tr[base].l != last) {
+      R += tr[tr[base].l].sz;
     }
-    else{
-	    pair<int, int> p = split(L, number);
-	    decart[father].l = p.second;
-	    p.second = father;
-	    update(father);
-	    return p;
-	}
+    if (last != -1 && tr[base].l != last) R++;
+    last = base;
+    base = tr[base].father;
+  }
+  return R;
 }
 
-int merge(int first, int second){ //merges two cartesians having roots first and second
-    if (first <= 0) return second;
-    if (second <= 0) return first;
-    if (decart[first].pr >= decart[second].pr){
-        int v = merge(decart[first].r, second);
-        decart[first].r = v;
-        update(first);
-        return first;
-    }
-    else{
-	    int v = merge(first, decart[second].l);
-	    decart[second].l = v;
-	    update(second);
-	    return second;
-	}
+void print(int v) { // print tree
+  if (v) {
+    print(tr[v].l);
+    cout << v << endl;
+    print(tr[v].r);
+  }
 }
 
-//DON`T FORGET THAT 0 is a fictitious vertex HERE.
-
-void init(){
-	decart[ptr++] = {-1, -1, rand()%1000000000, 0, -1}; //put a fictitious vertex with your parameters here
+void init() { // set init value here
+  tr[ptr++] = {0, 0, gen_priority(), 0, -1, 0};
 }
 
-int main()
-{
-	init();
+int main() {
+  init();
 }
-
