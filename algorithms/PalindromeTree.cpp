26a3932f361f00da078d08b1149780e5d6fada62
Denisson <shpakovskij.denis@yandex.ru>
2020-08-13 18:00:02 +0300

    Update PalindromeTree

--- algorithms/PalindromeTree.cpp
+++ algorithms/PalindromeTree.cpp
@@ -1,13 +1,26 @@
+// сжатые пути по diff[] не пересекаются, т.к. иначе палиндромы бы совпали
+// количество прыжков по jump[] есть O(logn)
+// для одного сжатого пути верно следующее:
+//   - палиндромы встречаются в строке сверху вниз по этому пути
+//   - прыжок наверх может быть только сразу до корня пути
+//   - т.е. можно поддерживать префикс пути, который встречался недавно до очередного прыжка в корень пути
+//   - для такого префикса последние вхождения палиндромов идут подряд с шагом в diff[]
+
 struct PalindromeTree {
     static const int SZ = 5e5;
     static const int SIGMA = 26;
 
     vector<int> s;
-    int to[SZ][SIGMA];
-    int suf[SZ];
-    int len[SZ];
-    int last;
-    int sz;
+    int to[SZ][SIGMA]; // переходы по каждой из букв
+    int suf[SZ];       // суфф ссылка
+    int len[SZ];       // длина палиндрома для вершины
+    int last;          // текущий самый длинный палиндром
+    int sz;            // количество вершин в дереве (число различных палиндромов + 2 корня)
+
+    int diff[SZ];      // diff[v] = len[v] - len[suf[v]];
+    int jump[SZ];      // прыжок через предков с одинаковым diff
+    int root[SZ];      // корень сжатого пути
+    int jump_len[SZ];  // длина сжатого прыжка
 
     // 0, 1 - roots
     PalindromeTree() {
@@ -45,6 +58,17 @@ struct PalindromeTree {
                 suf[v] = to[last][c];
             }
             last = v;
+
+            diff[v] = len[v] - len[suf[v]];
+            if (diff[v] == diff[suf[v]]) {
+              jump[v] = jump[suf[v]];
+              root[v] = root[suf[v]];
+              jump_len[v] = jump_len[suf[v]] + 1;
+            } else {
+              jump[v] = suf[v];
+              root[v] = v;
+              jump_len[v] = 0;
+            }
         } else {
             last = to[last][c];
         }
